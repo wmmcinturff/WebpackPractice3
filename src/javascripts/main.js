d65588ade26051ce184ce6288e8543e8ff2127e4
Wanda <wmmcinturff@gmail.com>
2020-09-05 09:43:17 -0500

    modified h1 and background

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,8 @@
 import '../styles/main.scss';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  $('#app').html('<h1>Who Is Your Daddy</h1>');
+  console.error('And What Does He Do?');
 };
 
 init();
